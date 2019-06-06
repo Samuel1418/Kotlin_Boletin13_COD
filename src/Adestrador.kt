0a294b367c86280c573809391b34c6164cf12c4b
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-06-06 09:11:28 +0200

    Comentada clase Adestrador

--- src/Adestrador.kt
+++ src/Adestrador.kt
@@ -5,6 +5,7 @@ class Adestrador(nome: String, apelidos: String, id: Int, edad: Int, var idFeder
 
     private fun dirixirPartido() {}
     private fun dirixirAdestramento() {}
+    //Los metodos va con fun y la llamada del nombre es como java
     override fun viaxar() {
         JOptionPane.showMessageDialog(null, "Viaxa o entrenador:" + super.nome)
     }
