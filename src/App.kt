1e64db2ddf09ec96b8b78e2c01e2c455a8946d8e
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-06-06 09:26:36 +0200

    Comentada clase main

--- src/App.kt
+++ src/App.kt
@@ -3,12 +3,15 @@ import javax.swing.JOptionPane
 
 fun main() {
 
+    //Se instancia directamente con una variable, llamando a la clase y haciendo el constructor.
+    //Se pone el nombre de los parametros y se van añadiendo
     val obx = Plantilla("Miguel", "Bazo", 10, 20)
     val obxX = Xogador("Roi", "Falcon", 7, 26, 18, "Delantero")
     val obxB = Adestrador("Carlos", "Jimenez", 23, 40, "753158p")
     val obxM = Masaxista("Juan", "Benitez", 30, 30, 6, "Fisioterapeuta")
     obx.concentrarse()
     obx.viaxar()
+    //Se llama el ToString igual que en java
     JOptionPane.showMessageDialog(null, obx.toString())
     obxX.viaxar()
     JOptionPane.showMessageDialog(null, obxX.toString())
