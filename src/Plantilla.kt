0a37cd5e441e21c3bba4b104dcd2af0f613fad57
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-06-06 09:15:46 +0200

    Comentada clase plantilla

--- src/Plantilla.kt
+++ src/Plantilla.kt
@@ -3,11 +3,11 @@ import javax.swing.JOptionPane
 
 
 open class Plantilla(var nome: String?, var apelidos: String?, var id: Int, var edad: Int) {
-//Con open se indica que es la clase padre.
+//Con open se indica que es la clase padre
     fun concentrarse() {
         JOptionPane.showMessageDialog(null, "Concentrase a seleccion.")
     }
-
+    //Tambien se indica con open el metodo que van heredar todas de la calse padre
     open fun viaxar() {
         JOptionPane.showMessageDialog(null, "Viaxa a seleccion")
     }
